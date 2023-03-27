63aea6c93188c1f1acf500f9e8b60bb71436c928
Mikey Molina <development@xircus.app>
2023-03-27 22:51:09 +0800

    Added with-erc20 example

--- examples/starter/contracts/TierRewardToken.sol
+++ examples/starter/contracts/TierRewardToken.sol
@@ -4,8 +4,3 @@ pragma solidity ^0.8.11;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@xircus-web3/contracts/interfaces/IXircusContract.sol";
 
-contract TierRewardToken is ERC20 {
-  constructor(uint256 initialSupply) ERC20("Gold", "GLD") {
-    _mint(msg.sender, initialSupply);
-  }
-}
